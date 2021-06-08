f6f1e5775feb1a0d4a40e1d23b2abc8e0d3695c0
Pitucha Aleksander <63605795+PituchaAleksander@users.noreply.github.com>
2021-06-08 12:03:40 +0200

    game/playerData class

--- source/game.py
+++ source/game.py
@@ -5,12 +5,16 @@ class game:
 
     def __init__(self):
         self.nick=""
+        self.score = 0
         self.character=''
         self.numberOfPlayers=0
         self.scoreboard=[]
 
     def writeAnswer(self):
         answers = playerData()
+        answers.nick=self.nick
+        answers.score=self.score
+
         print("Podaj odpowiednia liczbę aby wybrać opcje:\n1:państwo\n2:miasto\n3:roslina\n4:zwierze\n5:kolor\n6:imie\n7:wyświetl odpowiedzi\n8:zakończ")
         while True:
             x=int(input("Podaj liczbe: "))
@@ -30,15 +34,36 @@ class game:
                 print(answers.show())
             elif x==8:
                 break
+            self.ans=answers
 
-        return answers
+        return json.dumps(answers.__dict__)
 
     def showScoreAndAnswers(self):
         for i in range(0, self.numberOfPlayers):
             print(self.scoreboard[i].show())
+            if self.scoreboard[i].nick==self.nick:
+                self.score=self.scoreboard[i].score
 
     def calculateResults(self):
+
+        for i in range(0, self.numberOfPlayers):
+            self.scoreboard[i].calculateScore(self.character)
         print("Podaje wynik")
+
+    def getSocreBoardtoJson(self):
+        print("Zwracam Jsona")
+        json_string = json.dumps(self.scoreboard, default=obj_dict)
+
+        return json_string
+
+    def setSocreBoard(self):
+
+        print("Ustawiam tablice wyników")
+
+def obj_dict(obj):
+    return obj.__dict__
 
 g=game()
 print(g.writeAnswer())
+print(g.getJson())
+
