d6aba8ba81586c6b740d5934845ddb569cf17fe9
szymix1999 <zawsze.pod.wszystkim@gmail.com>
2021-06-15 09:37:49 +0200

    session id in host server

--- source/client.py
+++ source/client.py
@@ -21,48 +21,53 @@ def client_gameplay(host):
     server.connect((host[0], int(host[1])))
     server.sendall(("CONNECT " + player_data.nick + "\r\n").encode())
     try:
-        while True:
-            data = receive(server)
-            if "OK" in data:
-                host_name = data.split("OK ")[1]
-                print("Dołączyłeś do pokoju gracza " + host_name + "! Przejdź do interfejsu gry!")
-                app = GUIApp(host_name, player_data.nick)
+        data = receive(server)
+        if "OK" in data:
+            print(data)
+            host_name = data.split("OK ")[1]
+            print("Dołączyłeś do pokoju gracza " + host_name + "! Przejdź do interfejsu gry!")
+            app = GUIApp(host_name, player_data.nick)
 
-            elif "NEW_PLAYER" in data:
-                print("Gracz " + data.split("NEW_PLAYER ")[1].split("\r\n")[0] + " - dołączył do pokoju!")
+            while True:
+                data = receive(server)
+                if "NEW_PLAYER" in data:
+                    print("Gracz " + data.split("NEW_PLAYER ")[1].split("\r\n")[0] + " - dołączył do pokoju!")
 
-            elif "ROUND_START" in data:
-                while True:
-                    if app.is_created():
-                        break
-                curr_letter = data.split("ROUND_START ")[1].split(" ")[0]
-                round_time = data.split("ROUND_START ")[1].split(" ")[1] + " " + data.split("ROUND_START ")[1].split(" ")[2]
-                print("\nLitera: " + curr_letter)
-                app.start_game(curr_letter, round_time)
+                elif "ROUND_START" in data:
+                    while True:
+                        if app.is_created():
+                            break
+                    curr_letter = data.split("ROUND_START ")[1].split(" ")[0]
+                    round_time = data.split("ROUND_START ")[1].split(" ")[1] + " " + data.split("ROUND_START ")[1].split(" ")[2]
+                    print("\nLitera: " + curr_letter)
+                    app.start_game(curr_letter, round_time)
 
-            elif "END_ROUND" in data:
-                round_num += 1
-                player_data.categories = app.get_values()
-                app.set_letter("-")
-                print("Koniec rundy " + str(round_num) + "!")
-                app.set_warning("Koniec rundy!", "green")
-                server.sendall(("ANSWERS " + player_data.answers_to_pickle() + "\r\n").encode())
+                elif "END_ROUND" in data:
+                    round_num += 1
+                    player_data.categories = app.get_values()
+                    app.set_letter("-")
+                    print("Koniec rundy " + str(round_num) + "!")
+                    app.set_warning("Koniec rundy!", "green")
+                    server.sendall(("ANSWERS " + player_data.answers_to_pickle() + "\r\n").encode())
 
-            elif "RESULTS" in data:
-                results = data.split("RESULTS ")[1].split("\r\n")[0]
-                print("Wynik: ")
-                player_data.show_answers_and_save_score(results)
-                app.set_score(player_data.score)
-                app.set_warning("Oczekiwanie na hosta!", "blue")
-                print("Oczekiwanie na rozpoczęcie kolejnej rundy przez hosta!")
+                elif "RESULTS" in data:
+                    results = data.split("RESULTS ")[1].split("\r\n")[0]
+                    print("Wynik: ")
+                    player_data.show_answers_and_save_score(results)
+                    app.set_score(player_data.score)
+                    app.set_warning("Oczekiwanie na hosta!", "blue")
+                    print("Oczekiwanie na rozpoczęcie kolejnej rundy przez hosta!")
 
-            elif "END_GAME" in data:
-                print("Host opuścił pokój! Do zobaczenia następnym razem!")
-                server.close()
-                app.callback()
-                return
-            else:
-                print(data)
+                elif "END_GAME" in data:
+                    print("Host opuścił pokój! Do zobaczenia następnym razem!")
+                    server.close()
+                    app.callback()
+                    return
+                else:
+                    print(data)
+        else:
+            print(data)
+
     except socket.error:
         print("Host opuścił pokój! Do zobaczenia następnym razem!")
         server.close()
